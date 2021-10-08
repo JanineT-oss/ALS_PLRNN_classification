6dd4c51057ae0f4abc07d330f83d3b5ed8f75cf4
JanineT <59051486+JanineT-oss@users.noreply.github.com>
2021-10-08 13:55:26 +0200

    Add files via upload

---
+++ code_figures/figure_S2/convertLabD_FigS2.m
@@ -0,0 +1,14 @@
+function lab= convertLab(lab,nlab)
+lab=char(lab)
+
+for i=1:nlab
+   %ilab=lab(:,:,i);
+   ilab=lab(i,:);
+   ind= strfind(ilab,'D_');
+   ilab(ind:ind+1)=[];
+    ilab(ilab=='_')=[ ];
+    ind= strfind(ilab,'resA');
+   ilab(ind:ind+3)=[];
+   labs{i}=ilab;
+end
+lab=labs;
